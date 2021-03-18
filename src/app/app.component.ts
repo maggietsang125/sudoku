4f4f4c942004b2027a75971d56a7176c461a6d8a
Maggie <maggiecat218@gmail.com>
2021-03-18 22:12:12 +0800

    setup + trigger for loading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('loadingPage', [
+      state('open', style({
+        height: '100%',
+        opacity: 1,
+        backgroundColor: '#3498db'
+      })),
+      state('closed', style({
+        display: 'none',
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
   title = 'sudokuAng';
+
+  isOpen = true;
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
+
+
+
 }
